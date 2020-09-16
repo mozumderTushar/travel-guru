20c272a9d460683d02de1432f255c92758ae751c
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-09-16 15:17:22 +0600

    home page done

---
+++ src/components/SliderCarousel/SliderCarousel.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import './SliderCarousel.css';
+import fakeData from '../../fakeData/travelsDetails'
+import { Button } from '@material-ui/core';
+
+const SliderCarousel = () => {
+
+    const [index, setIndex] = useState(0);
+
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+    // console.log(index);
+
+    const [data, setData] = useState({})
+    useEffect(()=>{
+        const fake = fakeData.find(data => data.id === index)
+        setData(fake)
+    },[index])
+
+    return (
+        <div className="bg">
+            <div className="row">
+                <div className="col-md-8 carousel-details">
+                <h1>{data.name}</h1>
+                <h3>{data.details}</h3>
+                <Button className="button" height="40px" variant="contained">{data.button}</Button>
+                </div>
+                <div className="col-md-4">
+                    <Carousel activeIndex={index} onSelect={handleSelect}>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carousel-img"
+                                src="https://i.ibb.co/9gQZxBQ/Sajek.png"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>Cox's bazar</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carousel-img"
+                                src="https://i.ibb.co/YPKSjtc/Sreemongol.png"
+                                alt="Second slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Sreemongol</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100 carousel-img"
+                                src="https://i.ibb.co/2SD3HhX/sundorbon.png"
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Sundorbon</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SliderCarousel;
